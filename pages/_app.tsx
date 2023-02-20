492db72f177c18ce92043b4b0116b5b2658a4162
Byorn De Silva <byorn.desilva@gmail.com>
2023-02-20 20:11:25 +1100

    Update FrontView theme file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react';
+import Head from 'next/head';
+import Page from '../components/Page';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'aos/dist/aos.css';
 export default function App({ Component, pageProps }: AppProps) {
-  return <><Component {...pageProps} /><div>footer</div></>
+  return <React.Fragment>
+    <Head>
+      <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <title>Coding Tasker</title>
+    </Head>
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  </React.Fragment>
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,24 +5,45 @@ class MyDocument extends Document{
     render() {
         return<Html lang={"en"}>
             <Head>
-                <link
-                    rel="preload"
-                    href="/fonts/IBMPlexSans-Bold.ttf"
-                    as="font"
-                    crossOrigin="anonymous"
-                ></link>
-                <link
-                    rel="preload"
-                    href="/fonts/IBMPlexSans-Regular.ttf"
-                    as="font"
-                    crossOrigin="anonymous"
-                ></link>
-                <link
-                    rel="preload"
-                    href="/fonts/IBMPlexSans-SemiBold.ttf"
-                    as="font"
-                    crossOrigin="anonymous"
-                ></link>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <link
+                        rel="shortcut icon"
+                        href="https://assets.maccarianagency.com/favicons/thefront/favicon.ico"
+                    />
+                    <meta name="theme-color" content="#ffffff" />
+                    <meta
+                        name="description"
+                        content="A modern design system for your new landing and web pages."
+                    />
+                    <meta
+                        name="robots"
+                        content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+                    />
+                    <meta property="og:locale" content="en_US" />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:image"
+                        content="https://assets.maccarianagency.com/screenshots/the-front/social.png"
+                    />
+                    <meta
+                        property="og:title"
+                        content="theFront | UI Kit by Maccarian Agency."
+                    />
+                    <meta
+                        property="og:description"
+                        content="A modern design system for your new landing and web pages."
+                    />
+                    <meta
+                        property="og:url"
+                        content="https://thefront.maccarianagency.com/"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
             </Head>
             <body>
                 <Main />
